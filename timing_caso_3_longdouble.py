7577ccb7f528e27b952f8f28c2cd5e5be4d61a1a
raimolid <69227033+raimolid@users.noreply.github.com>
2020-08-12 20:06:08 -0400

    Update timing_caso_3_longdouble.py

--- timing_caso_3_longdouble.py
+++ timing_caso_3_longdouble.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 Ns=[2,5,10,12,15,20,30,40,45,50,55,60,75,100,125,160,
     200,250,350,500,600,800,1000,2000,5000,10000]
 
-#CREANDO ARCHIVO DE TEXTO 
+#CREANDO ARCHIVO D TEXTO 
 dts=[]
 mem=[]
 name=("timing_caso_3_longdouble.txt")
